4f71b92b96acf3c83c9081a06a86227e970c7ae2
David Beveridge <davidjbeveridge@gmail.com>
2013-08-10 11:51:48 -0700

    fix article links

--- helpers/artifact_helper.rb
+++ helpers/artifact_helper.rb
@@ -2,11 +2,11 @@ module ArtifactHelper
   require 'lib/artifact'
   
   def artifact_url(artifact)
-    url_for "artifacts/#{artifact.id}.html"
+    "artifacts/#{artifact.id}.html"
   end
   
   def artifact_content_url(artifact)
-    url_for "artifacts/#{artifact.id}_content.html"
+    "artifacts/#{artifact.id}_content.html"
   end
   
 end
