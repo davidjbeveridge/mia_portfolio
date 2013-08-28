a4f5c0a132b33bd9a26603872c4448946cee85bc
David Beveridge <davidjbeveridge@gmail.com>
2013-08-28 10:07:21 -0700

    Add Professional docs

--- helpers/section_helper.rb
+++ helpers/section_helper.rb
@@ -9,4 +9,25 @@ module SectionHelper
     section.title.underscore.gsub(/[\s_]+/, '-')
   end
   
+  def section_artifacts(section)
+    if section.artifact_count > 0
+      section.artifacts.map do |artifact|
+        partial(:artifact_grid_item, locals: {artifact: artifact})
+      end.join("\n")
+    else
+      ""
+    end
+  end
+  
+  def section_downloads(section)
+    if section.download_count > 0
+      section.downloads.map do |download|
+        partial(:download_grid_item, locals: {download: download})
+      end.join("\n")
+    else
+      ""
+    end
+  end
+  
+  
 end
--- lib/section.rb
+++ lib/section.rb
@@ -1,15 +1,24 @@
 class Section < BasicModel
 
   def attribute_names
-    %w(title artifacts)
+    %w(title artifacts downloads)
   end
   
   def artifacts
     @cached_artifacts ||= ( @attrs['artifacts'] || [] ).map { |slug| Artifact.load(slug) }
   end
+
+  def downloads
+    @cached_downloads ||= ( @attrs['downloads'] || [] )
+  end
+
   
   def artifact_count
     (@attrs['artifacts'] || []).count
+  end
+  
+  def download_count
+    (@attrs['downloads'] || []).count
   end
   
 end
